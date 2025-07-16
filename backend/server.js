79d8757a30c26344b1dd6672c60b8a5ce1c0d6bd
AniketChavan07 <aniketchavan00981gmail.com>
2025-07-16 10:10:01 +0530

    update commit

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("✅ Server is running!");
+  res.send(" Server is running!");
 });
 
 app.listen(PORT, () => {
